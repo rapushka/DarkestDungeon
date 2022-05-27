0967a1579d458b827517ef22fd91d67744b0fa04
Ja-baka <73071447+rapushka@users.noreply.github.com>
2022-05-27 10:17:39 +0300

    Bounded Camera

--- Assets/Scripts/PlayerChaser.cs
+++ Assets/Scripts/PlayerChaser.cs
@@ -4,7 +4,9 @@ public class PlayerChaser : MonoBehaviour
 {
     [SerializeField] private Player _player;
     [SerializeField] private float _movingSpeed;
-
+    [SerializeField] private Vector2 _minCameraPosition;
+    [SerializeField] private Vector2 _maxCameraPosition;
+    
     private Transform _playerTransform;
     private Vector3 _target;
 
@@ -23,8 +25,7 @@ public class PlayerChaser : MonoBehaviour
 
     private void Update()
     {
-        _target.x = _playerTransform.position.x;
-        _target.y = _playerTransform.position.y;
+        _target = GetBoundedCameraPosition();
 
         Vector3 position = Vector3.Lerp
         (
@@ -34,5 +35,22 @@ public class PlayerChaser : MonoBehaviour
         );
 
         transform.position = position;
+    }
+
+    private Vector3 GetBoundedCameraPosition()
+    {
+        float x = _playerTransform.position.x;
+        float y = _playerTransform.position.y;
+
+        float minX = _minCameraPosition.x;
+        float minY = _minCameraPosition.y;
+        float maxX = _maxCameraPosition.x;
+        float maxY = _maxCameraPosition.y;
+
+        Vector3 target = Vector3.zero;
+        target.x = Mathf.Max(Mathf.Min(x, maxX), minX);
+        target.y = Mathf.Max(Mathf.Min(y, maxY), minY);
+        target.z = -10;
+        return target;
     }
 }
